1b7f06475c0826b2c6f33c3bcc91f58aaa5a2924
Anil <33496906+anilkda@users.noreply.github.com>
2024-03-13 10:58:51 +0530

    Amplify init done

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "writewithme": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  }
+}
